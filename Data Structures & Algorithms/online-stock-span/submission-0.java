0aef894ae22950c2514442f57a871ca319c245a5
Roshan Giri <roshangiri711@gmail.com>
2026-08-18 15:21:33 +0530

    Add: online-stock-span - submission-0

---
+++ Data Structures & Algorithms/online-stock-span/submission-0.java
@@ -0,0 +1,30 @@
+class StockSpanner {
+    List<Integer> arr;
+    public StockSpanner() {
+        arr = new ArrayList<>();
+    }
+
+    public int next(int price) {
+        if (arr.size() == 0) {
+            arr.add(price);
+            return 1;
+        }
+        arr.add(price);
+        int compare = price;
+        int count= 0;
+        for(int i= arr.size()-1; i>= 0; i--){
+            if(arr.get(i)<= compare){
+                count++;
+            }else{
+                break;
+            }
+        }
+        return count;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
