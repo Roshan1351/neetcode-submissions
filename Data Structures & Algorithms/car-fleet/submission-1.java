e2a6f9c7a3a28d5979cdbcb142d9c0bd010af493
Roshan Giri <roshangiri711@gmail.com>
2026-08-18 16:00:55 +0530

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.java
@@ -0,0 +1,32 @@
+class pairs{
+    int position;
+    int speed;
+
+    pairs(int position, int speed){
+        this.position= position;
+        this.speed= speed;
+    }
+}
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        List<pairs> list= new ArrayList<>();
+        for(int i= 0; i<position.length; i++){
+            list.add(new pairs(position[i], speed[i]));
+        }
+
+        list.sort((a, b)->a.position-b.position);
+
+        int fleet= 0;
+        double maxTime= 0;
+
+        for(int i= list.size()-1; i>= 0; i--){
+            double time= (double)(target-list.get(i).position)/list.get(i).speed;
+
+            if(time>maxTime){
+                fleet++;
+                maxTime= time;
+            }
+        }
+        return fleet;
+    }
+}
